5e6223974cba48011d30a34fc7f073e1854f4b66
Mxolisi Khumalo <mxkhumajhb024@student.wethinkcode.co.za>
2025-09-13 16:35:41 +0200

    commiting files to repo

---
+++ README.txt
@@ -0,0 +1,28 @@
+# ConcurrencyAssignment2023
+# Project Name: Club Simulation
+
+## Description
+
+The club simulation program simulates a club environment, much like in real life. 
+
+## Features
+
+- **Club Goer:** A club goer can enter the club, unless the maximum capacity is reached, then it must wait outside the club.
+                 Once inside the club, a club goer can get drinks from the bar, wander around, or dance on the dance floor.
+                 If a club goer leaves, a club goer may enter from the waiting line, thus reaching the maximum specified limit once again.
+                 
+## Usage
+
+1. Download the file to a local storage space on your PC
+2. Change the command line inputs in the 'run' method of the makefile, if you wish
+3. Run the make file using the terminal command 'make run' to open the simulation window
+4. Click on the 'start' button to begin the simulation
+5. Use the 'pause' button to pause the simulation. This will change to a 'resume' button to allow you to resume the simulation
+6. Click the 'quit' button to end the simulation and the program
+
+
+## Credits
+
+- Developed by [Mxolisi Khumalo]
+
+---
---
+++ src/clubSimulation/PeopleCounter.java
@@ -0,0 +1,69 @@
+package clubSimulation;
+
+import java.util.concurrent.atomic.*;
+
+public class PeopleCounter {
+	private AtomicInteger peopleOutSide; // counter for people arrived but not yet in the building
+	private AtomicInteger peopleInside; // counter for patrons inside club
+	private AtomicInteger peopleLeft; // counter for patrons who have left the club
+	private AtomicInteger maxPeople; // maximum patrons allowed in the club at one time
+
+	PeopleCounter(int max) {
+		peopleOutSide = new AtomicInteger(0);
+		peopleInside = new AtomicInteger(0);
+		peopleLeft = new AtomicInteger(0);
+		maxPeople = new AtomicInteger(max);
+	}
+
+	public synchronized int getWaiting() {
+		return peopleOutSide.get();
+	}
+
+	public synchronized int getInside() {
+		return peopleInside.get();
+	}
+
+	public synchronized int getTotal() {
+		return (peopleOutSide.get() + peopleInside.get() + peopleLeft.get());
+	}
+
+	public synchronized int getLeft() {
+		return peopleLeft.get();
+	}
+
+	public synchronized int getMax() {
+		return maxPeople.get();
+	}
+
+	// someone arrived outside
+	public synchronized void personArrived() {
+		peopleOutSide.getAndIncrement();
+	}
+
+	// someone got inside
+	public synchronized void personEntered() {
+		peopleOutSide.getAndDecrement();
+		peopleInside.getAndIncrement();
+	}
+
+	// someone left
+	public synchronized void personLeft() {
+		peopleInside.getAndDecrement();
+		peopleLeft.getAndIncrement();
+
+	}
+
+	// too many people inside
+	public synchronized boolean overCapacity() {
+		if (peopleInside.get() >= maxPeople.get())
+			return true;
+		return false;
+	}
+
+	// not used
+	public synchronized void resetScore() {
+		peopleInside.set(0);
+		peopleOutSide.set(0);
+		peopleLeft.set(0);
+	}
+}
